7470eb4fda8363902d44f36643d71cc20bf83ed5
Archita Racherla <archita.racherla@gmail.com>
2021-08-29 14:20:35 -0500

    fixed back to dashboard button

--- client/src/components/Inbox/index.js
+++ client/src/components/Inbox/index.js
@@ -6,11 +6,12 @@
 // import { useAlert } from 'react-alert';
 // import FileBase64 from "react-file-base64";
 // import gql from "graphql-tag";
-import "../../assets/styles/editbike.css";
+//import "../../assets/styles/editbike.css";
 import React, { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useQuery } from "@apollo/client";
 import { QUERY_MESSAGES_FROM_USER } from "../../utils/queries";
+import "../../assets/styles/inbox.css";
 
 const Inbox = () => {
   const [open, setOpen] = useState(false);
@@ -84,19 +85,14 @@ const Inbox = () => {
                         as="h3"
                         className="text-lg addheadertext leading-6 font-medium text-gray-900"
                       >
-                        Your Messages
+                        Inbox
                       </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Please add or edit details to your bike, if need be!
-                        </p>
-                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="mt-5 md:mt-0 md:col-span-2">
-                  <div className="inline-block addbikecontainer align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                    <div className="mt-5 md:mt-0 md:col-span-2">
+                  <div className="">
+                    <div className="replies mt-5 md:mt-0 md:col-span-2">
                       {messagesWithReplies &&
                         messagesWithReplies.map((message) => (
                           <div key={message._id}>
@@ -117,7 +113,7 @@ const Inbox = () => {
                         ))}
                       <button
                         type="button"
-                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                        className="backButton mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                         onClick={() => setOpen(false)}
                         ref={cancelButtonRef}
                       >
